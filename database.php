a00071f7ffc5e2a3ce7575f7f1713c9229cdc34d
Thomas Andrei <thomasandrei1991@gmail.com>
2026-08-09 23:41:27 +0800

    Refactoring Database

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
     $host="localhost";
     $user="root";
     $pass="";
-    $db="hrms_db";
+    $db="hrms_db_new";
     $conn=mysqli_connect($host,$user,$pass,$db);
     if(!$conn){
         die("Connection Failed");
---
+++ generate_password.php
@@ -0,0 +1,7 @@
+<?php
+
+$password = "123456";
+
+echo password_hash($password, PASSWORD_DEFAULT);
+
+?>
